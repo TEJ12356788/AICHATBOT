cccb236ddc5d29360057c4f8505b7777572889b1
TEJ12356788 <164802726+TEJ12356788@users.noreply.github.com>
2024-03-25 16:55:11 +0400

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def main():
         vector_store.save_local("faiss_index")
 
     # Function to load and configure the conversational chain
-   from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 
