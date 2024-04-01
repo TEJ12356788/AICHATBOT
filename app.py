94c56e2c84ff529147b5b7d7649f2ed24e22ad72
TEJ12356788 <164802726+TEJ12356788@users.noreply.github.com>
2024-04-01 23:25:17 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PyPDF2 import PdfReader
+from dotenv import load_dotenv
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
