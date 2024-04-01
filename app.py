4d083dcb3041b84026437bb44f1f77aec2bd0e91
TEJ12356788 <164802726+TEJ12356788@users.noreply.github.com>
2024-04-01 23:18:55 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,71 +1,106 @@
 import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+import google.ai.generativelanguage as glm
+from dotenv import load_dotenv
+from PIL import Image
+import os
+import io
+
+load_dotenv()
+
+def image_to_byte_array(image: Image) -> bytes:
+    imgByteArr = io.BytesIO()
+    image.save(imgByteArr, format=image.format)
+    imgByteArr = imgByteArr.getvalue()
+    return imgByteArr
+
+API_KEY = os.environ.get("GOOGLE_API_KEY")
+genai.configure(api_key=API_KEY)
+
+gemini_pro, gemini_vision, ai_chat_tab = st.columns([1, 1, 2])
 
 def main():
-    st.title("Text and PDF Chat")
+    with gemini_pro:
+        st.header("Interact with Parvazbot Pro")
+        st.write("")
 
-    # Function to extract text from PDF documents
-    def get_pdf_text(pdf_docs):
-        text = ""
-        for pdf in pdf_docs:
-            try:
-                pdf_reader = PdfReader(pdf)
-                for page in pdf_reader.pages:
-                    text += page.extract_text()
-            except Exception as e:
-                if type(e).__name__ == "PdfReadError":
-                    st.warning(f"Skipping non-PDF file: {pdf.name}. Error: {str(e)}")
-                    continue
+        prompt = st.text_input("Chat please...", placeholder="Prompt", label_visibility="visible")
+        model = genai.GenerativeModel("gemini-pro")
+
+        if st.button("ENTER", use_container_width=True):
+            response = model.generate_content(prompt)
+
+            st.write("")
+            st.header(":blue[Response]")
+            st.write("")
+
+            st.markdown(response.text)
+
+    with gemini_vision:
+        st.header("Interact with Gemini Pro Vision")
+        st.write("")
+
+        image_prompt = st.text_input("Interact with the Image", placeholder="Prompt", label_visibility="visible")
+        uploaded_file = st.file_uploader("Choose an Image", accept_multiple_files=False,
+                                         type=["png", "jpg", "jpeg", "img", "webp"])
+
+        if uploaded_file is not None:
+            st.image(Image.open(uploaded_file), use_column_width=True)
+
+            st.markdown("""
+                <style>
+                        img {
+                            border-radius: 10px;
+                        }
+                </style>
+                """, unsafe_allow_html=True)
+
+        if st.button("GET RESPONSE", use_container_width=True):
+            model = genai.GenerativeModel("gemini-pro-vision")
+
+            if uploaded_file is not None:
+                if image_prompt != "":
+                    image = Image.open(uploaded_file)
+
+                    response = model.generate_content(
+                        glm.Content(
+                            parts=[
+                                glm.Part(text=image_prompt),
+                                glm.Part(
+                                    inline_data=glm.Blob(
+                                        mime_type="image/jpeg",
+                                        data=image_to_byte_array(image)
+                                    )
+                                )
+                            ]
+                        )
+                    )
+
+                    response.resolve()
+
+                    st.write("")
+                    st.write(":blue[Response]")
+                    st.write("")
+
+                    st.markdown(response.text)
+
                 else:
-                    raise  # Raise the exception if it's not a PdfReadError
-        return text
+                    st.write("")
+                    st.header(":red[Please Provide a message]")
 
-    # Function for PDF chat functionality
-    def pdf_chat():
-        st.header("PDF Chat")
+            else:
+                st.write("")
+                st.header(":red[Please Provide an image]")
 
-        # Option to upload PDF files or prompt a question
-        option = st.radio("Choose an option", ["Upload PDF files", "Prompt a question"])
-
-        if option == "Upload PDF files":
-            # Allow user to upload PDF files
-            pdf_docs = st.file_uploader("Upload PDF files", accept_multiple_files=True)
-            if st.button("Submit & Process"):
-                with st.spinner("Processing..."):
-                    raw_text = get_pdf_text(pdf_docs)
-                    # Process the extracted text
-                    st.success("PDF files processed successfully")
-
-        elif option == "Prompt a question":
-            # Allow user to input a question
-            user_question = st.text_input("Ask a question about the PDF files")
-            if user_question:
-                if st.button("Ask"):
-                    # Process the user question
-                    st.write("Response:")
-
-    # Function for AI chat functionality
-    def ai_chat():
+    with ai_chat_tab:
         st.header("AI Chat")
 
         user_input = st.text_input("You:", "")
         if st.button("Send"):
             if user_input.strip() != "":
-                model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
-                response = model(user_input)
-                st.text_area("AI:", value=response, height=200)
-
-    # Display the PDF chat functionality
-    pdf_chat()
-
-    # Display the AI chat functionality
-    ai_chat()
+                model = genai.GenerativeModel("gemini-pro", temperature=0.3)
+                response = model.generate_content(user_input)
+                st.text_area("AI:", value=response.text, height=200)
 
 if __name__ == "__main__":
     main()
